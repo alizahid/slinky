886429d74c8b09055b5ff7da52c7e40014247ebf
Ali Zahid <ali.zahid@live.com>
2016-08-19 22:51:06 +0500

    Too hardcore for comments

--- assets/js/jquery.slinky.js
+++ assets/js/jquery.slinky.js
@@ -9,8 +9,6 @@
 	var lastClick;
 
 	$.fn.slinky = function(options) {
-		// Setup plugin defaults
-
 		var settings = $.extend({
 			label: 'Back',
 			title: false,
@@ -18,52 +16,32 @@
 			resize: true
 		}, options);
 
-		// Convenience method for navigation animation
-
 		var move = function(menu, next, callback) {
 			var left = Math.round(parseInt(menu.get(0).style.left)) || 0;
 
-			// Use multiples of 100% for responsive animation
-
 			menu.css('left', -Math.abs(next ? left - 100 : left + 100) + '%');
-
-			// Callback after animation is finished
 
 			if (typeof callback === 'function') {
 				setTimeout(callback, settings.speed);
 			}
 		};
 
-		// Convenience method for resizing menu
-
 		var resize = function(menu, content) {
 			menu.height(content.outerHeight());
 		};
 
 		return this.each(function() {
-			// The root node is where animation happens
-
 			var menu = $(this),
 				root = menu.children().first();
-
-			// Set CSS animation duration
 
 			menu.css('transition-duration', settings.speed + 'ms');
 			root.css('transition-duration', settings.speed + 'ms');
 
-			// Add .next class to links with sub menus
-
 			$('a + ul', menu).prev().addClass('next');
-
-			// Add header for back button and title
 
 			$('li > ul', menu).prepend('<li class="header">');
 
-			// Add title
-
 			if (settings.title === true) {
-				// Create a label with title from the parent
-
 				$('li > ul', menu).each(function() {
 					var label = $(this).parent().find('a').first().text(),
 						title = $('<h2>').text(label);
@@ -72,11 +50,7 @@
 				});
 			}
 
-			// Add back links with appropriate labels
-
 			if (!settings.title && settings.label === true) {
-				// Create a link with label from parent
-
 				$('li > ul', menu).each(function() {
 					var label = $(this).parent().find('a').first().text(),
 						backLink = $('<a>').text(label).prop('href', '#').addClass('back');
@@ -84,14 +58,10 @@
 					$('> .header', this).append(backLink);
 				});
 			} else {
-				// Create a link with the label from settings
-
 				var backLink = $('<a>').text(settings.label).prop('href', '#').addClass('back');
 
 				$('.header', menu).append(backLink);
 			}
-
-			// Setup navigation
 
 			$('a', menu).on('click', function(e) {
 				if ((lastClick + settings.speed) > Date.now()) {
@@ -102,15 +72,9 @@
 
 				var a = $(this);
 
-				// Disable navigation if link has hash
-				// else proceed to URL
-
 				if (/#/.test(this.href)) {
 					e.preventDefault();
 				}
-
-				// Animate forward or backward
-				// Resize menu height to match content, if required
 
 				if (a.hasClass('next')) {
 					a.next().show();
