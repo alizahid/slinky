618469ae74856980bc6027f12673e4a6f1ff51a7
Ali Zahid <ali.zahid@live.com>
2018-01-08 10:04:34 +0400

    Copy files to /docs

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,28 +8,34 @@ import sourcemaps from 'gulp-sourcemaps'
 import uglify from 'gulp-uglify'
 
 gulp.task('slinky-js', () =>
-	gulp
-		.src('src/slinky.js')
-		.pipe(sourcemaps.init())
-		.pipe(babel())
-		.pipe(uglify())
-		.pipe(rename('slinky.min.js'))
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('dist'))
+  gulp
+    .src('src/slinky.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(rename('slinky.min.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist'))
 )
 
 gulp.task('slinky-css', () =>
-	gulp
-		.src('src/slinky.scss')
-		.pipe(sass.sync().on('error', sass.logError))
-		.pipe(autoprefixer())
-		.pipe(rename('slinky.min.css'))
-		.pipe(gulp.dest('dist'))
+  gulp
+    .src('src/slinky.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(rename('slinky.min.css'))
+    .pipe(gulp.dest('dist'))
 )
 
-gulp.task('default', ['slinky-js', 'slinky-css'])
+gulp.task('docs', () =>
+  gulp
+    .src(['dist/slinky.min.css', 'dist/slinky.min.js', 'dist/slinky.min.js.map'])
+    .pipe(gulp.dest('docs/slinky'))
+)
+
+gulp.task('default', ['slinky-js', 'slinky-css', 'docs'])
 
 gulp.task('dev', ['slinky-js', 'slinky-css'], () => {
-	gulp.watch('src/slinky.js', ['slinky-js'])
-	gulp.watch('src/slinky.scss', ['slinky-css'])
+  gulp.watch('src/slinky.js', ['slinky-js'])
+  gulp.watch('src/slinky.scss', ['slinky-css'])
 })
